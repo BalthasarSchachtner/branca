60621ba641e7c008f4a83623469eb267bac9e877
Balthasar Schachtner <Balthasar.Schachtner@posteo.de>
2019-07-14 15:16:27 +0200

    make the legend larger

--- branca/templates/color_scale.js
+++ branca/templates/color_scale.js
@@ -13,26 +13,31 @@
 
     {{this.get_name()}}.x = d3.scale.linear()
               .domain([{{ this.color_domain[0] }}, {{ this.color_domain[-1] }}])
-              .range([0, 400]);
+              .range([0, 800]);
 
     {{this.get_name()}}.legend = L.control({position: 'topright'});
     {{this.get_name()}}.legend.onAdd = function (map) {var div = L.DomUtil.create('div', 'legend'); return div};
     {{this.get_name()}}.legend.addTo({{this._parent.get_name()}});
 
+
     {{this.get_name()}}.xAxis = d3.svg.axis()
         .scale({{this.get_name()}}.x)
         .orient("top")
-        .tickSize(1)
+        .tickSize(2)
         .tickValues({{ this.tick_labels }});
 
     {{this.get_name()}}.svg = d3.select(".legend.leaflet-control").append("svg")
         .attr("id", 'legend')
-        .attr("width", 450)
-        .attr("height", 40);
+        .attr("width", 900)
+        .attr("height", 120)
+        .attr("transform", "translate(0,68)");
 
     {{this.get_name()}}.g = {{this.get_name()}}.svg.append("g")
         .attr("class", "key")
-        .attr("transform", "translate(25,16)");
+        .attr("transform", "translate(50,32)")
+        .attr('class', 'axis')
+        .attr("font-size", "250%");
+
 
     {{this.get_name()}}.g.selectAll("rect")
         .data({{this.get_name()}}.color.range().map(function(d, i) {
@@ -43,13 +48,14 @@
           };
         }))
       .enter().append("rect")
-        .attr("height", 10)
+        .attr("height", 20)
         .attr("x", function(d) { return d.x0; })
-        .attr("width", function(d) { return d.x1 - d.x0; })
+        .attr("width", function(d) { return (d.x1 - d.x0); })
         .style("fill", function(d) { return d.z; });
 
     {{this.get_name()}}.g.call({{this.get_name()}}.xAxis).append("text")
         .attr("class", "caption")
-        .attr("y", 21)
+        .attr("y", 55)
+        .attr("font-size", "100%")
         .text('{{ this.caption }}');
 {% endmacro %}
